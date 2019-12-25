46923b2129eb6e7f7d2e667dfff723c7eb676bbf
diodonfrost <diodon.frost@diodonfrost.me>
2019-12-26 00:29:50 +0100

    removing provider files

--- 02-instance-with-volume/provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure credential OpenStack Provider
-provider "openstack" {
-  user_name   = "my-litle-user"
-  tenant_name = "my-little-tenant"
-  password    = "secret"
-  auth_url    = "http://your-cloud-prodivder.com"
-}
-
